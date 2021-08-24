8f3d74dc3149e7d53f838aa5e9f42aaa9258d7b3
Valmyr Tavares <valmyrtavares71@gmail.com>
2021-08-24 12:15:50 -0300

    SetImg done

--- js/module/slide.js
+++ js/module/slide.js
@@ -4,8 +4,25 @@ export default class slide{
         this.img = document.querySelectorAll("img")
     }
 
+    setImg(){
+       let limit = this.img.length -1;
+       console.log("limit =  " +  limit)
+       let count = 0
+       this.img[limit].classList.add('show');
+      setInterval(()=>{          
+       this.img.forEach(item=>{
+           item.classList.remove('show');
+       })
+    count++
+    if(count>limit)
+    {count = 0} ;
+     this.img[count].classList.add('show');
+    console.log(count)
+      },2000)
+    }
+
     init(){
-        console.log(this.img)
+        this.setImg();
     }
 }
 
