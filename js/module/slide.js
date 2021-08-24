b2b33bb0f2e2041acf19ee4c1ba4de0ba77bbe27
Valmyr Tavares <valmyrtavares71@gmail.com>
2021-08-24 12:53:45 -0300

    clearInteval does not work

--- js/module/slide.js
+++ js/module/slide.js
@@ -3,32 +3,39 @@ export default class slide{
     constructor(){
         this.img = document.querySelectorAll("img");
         this.btn = document.querySelectorAll("button")
+         this.count = 0
+         this.stop;
     }
     addEvent(){
         this.btn[0].addEventListener('click', this.back);
         this.btn[1].addEventListener('click', this.forward);       
     }
     back(){
-        console.log("O botão está funcionando BACK")
+      clearInterval(this.stopSlide)
+      console.log("Volta")
     }
+      
     forward(){
-        console.log("O botão está funcionando FORWARD")
+        clearInterval(this.stopSlide)
+        console.log("Frente")
+   
+       
     }
 
 
     setImg(){
-       let limit = this.img.length -1;     
-       let count = 0
+       let limit = this.img.length -1;      
        this.img[limit].classList.add('show');
-      setInterval(()=>{          
-       this.img.forEach(item=>{
-           item.classList.remove('show');
-       })
-    count++
-    if(count>limit)
-    {count = 0} ;
-     this.img[count].classList.add('show');  
-      },2000)
+
+     this.stopSlide =  setInterval(()=>{          
+                    this.img.forEach(item=>{
+                        item.classList.remove('show');
+                    })
+                        this.count++
+                        if(this.count>limit)
+                        {this.count = 0} ;
+                        this.img[this.count].classList.add('show');  
+                    },2000)
     }
 
     init(){
